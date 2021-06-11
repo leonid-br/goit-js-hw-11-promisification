835a887484427741f69778f4587d3c79768078bd
unknown <74738757+leonid-br@users.noreply.github.com>
2021-06-11 17:44:36 +0300

    Задание 2 - Промисификация

---
+++ task2.js
@@ -0,0 +1,29 @@
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+    const updatedUsers = allUsers.map(user =>
+        user.name === userName ? { ...user, active: !user.active } : user,
+        );
+    return Promise.resolve(updatedUsers);
+    
+};
+
+const loggerUser = updatedUsers => console.table(updatedUsers);
+
+toggleUserState(users, 'Mango').then(loggerUser);
+toggleUserState(users, 'Lux').then(loggerUser);
+
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
