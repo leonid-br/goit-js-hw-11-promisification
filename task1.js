a38264d38490903cf3349b10c2079c3966d1e81f
unknown <74738757+leonid-br@users.noreply.github.com>
2021-06-13 13:20:46 +0300

    правки

--- task1.js
+++ task1.js
@@ -1,7 +1,8 @@
 const delay = ms => {
- return Promise.resolve(ms)
+  return new Promise(resolve => {
+    setTimeout(() => {resolve(ms)}, ms);
+  });
 };
-
 const logger = time => console.log(`Resolved after ${time}ms`);
 
 // Вызовы функции для проверки
